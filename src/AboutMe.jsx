8b54d940b419ff168e3b0718c2fa4e412dd0ad91
Qadamboy-Xudayberganov <168646093+Qadamboy-Xudayberganov@users.noreply.github.com>
2025-04-03 22:48:03 +0500

    a

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,10 +1,10 @@
 const AboutMe = () => {
   return (
-    <div className="text-white max-w-[700px] m-auto py-[100px] flex flex-col items-center gap-[50px] relative z-1">
-      <h1 className="font-bold text-[48px] bg-gradient-to-r from-[#9C83FF] to-[#FF9051] bg-clip-text text-transparent">
+    <div className="text-white max-w-[700px] px-[30px] m-auto py-[100px] flex flex-col items-center gap-[50px] relative z-1 max-r600:px-[20px] max-r600:gap-[40px]">
+      <h1 className="font-bold text-[48px] bg-gradient-to-r from-[#9C83FF] to-[#FF9051] bg-clip-text text-transparent max-r600:text-[40px]">
         ABOUT ME
       </h1>
-      <div className="text-center text-[18px] flex flex-col gap-[30px] text-[E1E1E1]">
+      <div className="text-[18px] flex flex-col gap-[30px] text-[E1E1E1] max-r600:text-[15px]">
         <p>
           As a passionate software engineer, I thrive on the intricate dance
           between logic and creativity. Currently immersed in the dynamic world
