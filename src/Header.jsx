fd378fb5ceda84e0e2f8df09ce60127dbe9bede9
Qadamboy-Xudayberganov <168646093+Qadamboy-Xudayberganov@users.noreply.github.com>
2025-04-03 22:24:37 +0500

    a

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,15 +13,15 @@ const Header = () => {
           alt=""
         />
       </a>
-      <div className="w-[250px] flex justify-between mx-[100px] max-r900:w-[210px] max-r900:mx-[50px] max-r600:w-[180px] max-r600:mx-[30px] max-r450:w-[170px]">
+      <div className="w-[250px] flex justify-between mx-[100px] max-r900:w-[210px] max-r900:mx-[50px] max-r600:w-[180px] max-r600:mx-[30px] max-r450:w-[170px] max-r450:mx-[20px]">
         <a
-          className="text-white px-[25px] py-[8px] flex items-center justify-center border-white border rounded-[8px] hover:bg-white hover:text-black transition-all hover:scale-105 max-r900:px-[20px] max-r900:text-[15px] max-r900:py-[6px] max-r600:px-[16px] max-r600:text-[13px] max-r600:py-[4px] max-r600:rounded-[6px] max-r450:px-[10px]"
+          className="text-white px-[25px] py-[8px] flex items-center justify-center border-white border rounded-[8px] hover:bg-white hover:text-black transition-all hover:scale-105 max-r900:px-[20px] max-r900:text-[15px] max-r900:py-[6px] max-r600:px-[16px] max-r600:text-[13px] max-r600:rounded-[6px] max-r450:px-[12px]"
           href="#aboutMe"
         >
           About me
         </a>
         <a
-          className="bg-white px-[25px] py-[8px] flex items-center justify-center rounded-[8px] border-white border hover:bg-transparent hover:text-white transition-all hover:scale-105 max-r900:px-[20px] max-r900:text-[15px] max-r900:py-[6px] max-r600:px-[16px] max-r600:text-[13px] max-r600:py-[4px] max-r600:rounded-[6px] max-r450:px-[10px]"
+          className="bg-white px-[25px] py-[8px] flex items-center justify-center rounded-[8px] border-white border hover:bg-transparent hover:text-white transition-all hover:scale-105 max-r900:px-[20px] max-r900:text-[15px] max-r900:py-[6px] max-r600:px-[16px] max-r600:text-[13px] max-r600:rounded-[6px] max-r450:px-[12px]"
           href="#contact"
         >
           Contact
